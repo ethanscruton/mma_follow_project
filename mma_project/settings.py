2d949d6af26e154be7478ff5d2179c3876cc6313
Ethan <ethanscruton11@gmail.com>
2022-03-04 13:05:46 -0500

    commenting out STATICFILES_DIRS

--- mma_project/settings.py
+++ mma_project/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [STATIC_DIR, ]
+#STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = 'static/'
 
 # Default primary key field type
