c6298fe1dcd5a9d80b60cbec950cadb0afec06e6
Ethan <ethanscruton11@gmail.com>
2022-03-08 15:47:01 -0500

    debug true

--- mma_project/settings.py
+++ mma_project/settings.py
@@ -32,7 +32,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://mma-project.herokuapp.com/']
 
