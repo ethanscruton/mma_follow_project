2b5e992d16eb2eb70dbd2bf6a85289fb4fa7fa54
Ethan <ethanscruton11@gmail.com>
2022-03-10 10:54:35 -0500

    celery changes

--- mma_project/celery.py
+++ mma_project/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from decouple import config
-from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mma_project.settings')
 app = Celery('mma_project')
+
+from decouple import config
+from celery import Celery
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
