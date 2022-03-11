4ae19753bfb28841bacac3b8a3a9826953c3c4e3
Ethan <ethanscruton11@gmail.com>
2022-03-11 15:28:55 -0500

    allowed hosts

--- mma_project/settings.py
+++ mma_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['mmafollow.com', 'mma-project.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['www.mmafollow.com', 'mmafollow.com', 'mma-project.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
