694adaea1b61bae14a66b835a569328deccf13f8
Ethan <ethanscruton11@gmail.com>
2022-03-11 16:04:23 -0500

    favicon

--- mma_project/settings.py
+++ mma_project/settings.py
@@ -33,7 +33,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['www.mmafollow.com', 'mmafollow.com', 'mma-project.herokuapp.com', '127.0.0.1']
 
 
