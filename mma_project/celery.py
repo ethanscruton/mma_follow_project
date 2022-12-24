cf76aadc7ef3ced6129f86b620fe968e8700193f
Ethan <ethanscruton11@gmail.com>
2022-12-24 16:06:22 -0500

    update BROKER_URL

--- mma_project/celery.py
+++ mma_project/celery.py
@@ -20,4 +20,4 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 
 app.autodiscover_tasks()
-app.conf.update(BROKER_URL=config('REDIS_URL'))
+app.conf.update(BROKER_URL=config('CLOUDAMQP_URL'))
